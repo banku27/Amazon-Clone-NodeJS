0f6f8fd204219cb4f551a2f4972f4bd2c8ffd7f2
banku27 <pankajparihar123321@gmail.com>
2023-07-15 16:44:49 +0530

    auth screens

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:amazon_clone_nodejs/constants/global_variables.dart';
+import 'package:amazon_clone_nodejs/router.dart';
+import 'package:flutter/material.dart';
+
+import 'features/auth/screens/auth_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      onGenerateRoute: (settings) => generateRoute(settings),
+      debugShowCheckedModeBanner: false,
+      title: 'Amazone Clone',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        appBarTheme: const AppBarTheme(
+            elevation: 0,
+            iconTheme: IconThemeData(
+              color: Colors.black,
+            )),
+        primarySwatch: Colors.blue,
+        useMaterial3: true,
+      ),
+      home: const AuthScreen(),
+    );
+  }
+}
