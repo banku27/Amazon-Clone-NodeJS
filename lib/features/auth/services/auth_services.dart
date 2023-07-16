8ef0c7524fcd02b06879be6bde2d690ae87a947d
banku27 <pankajparihar123321@gmail.com>
2023-07-16 16:56:09 +0530

    sign up api and auth services

--- lib/features/auth/services/auth_services.dart
+++ lib/features/auth/services/auth_services.dart
@@ -1,30 +1,49 @@
+import 'package:amazon_clone_nodejs/constants/error_handling.dart';
 import 'package:amazon_clone_nodejs/constants/global_variables.dart';
+import 'package:amazon_clone_nodejs/constants/utils.dart';
 import 'package:amazon_clone_nodejs/models/user.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
 class AuthService {
   void signUpUser({
+    required BuildContext context,
     required String email,
     required String password,
     required String name,
   }) async {
     try {
       User user = User(
-          id: '',
-          name: name,
-          password: password,
-          email: email,
-          type: '',
-          address: '',
-          token: '');
+        id: '',
+        name: name,
+        password: password,
+        email: email,
+        address: '',
+        type: '',
+        token: '',
+      );
 
-      http.Response res = await http.post(Uri.parse('$uri/api/signup'),
-          headers: <String, String>{
-            'Content-Type': 'application-json; charset=UTF-8'
-          },
-          body: {
-            user.toJson()
-          });
-    } catch (e) {}
+      http.Response res = await http.post(
+        Uri.parse('$uri/api/signup'),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+      );
+
+      // ignore: use_build_context_synchronously
+      httpErrorHandle(
+        response: res,
+        context: context,
+        onSuccess: () {
+          showSnackBar(
+            context,
+            'Account created! Login with the same credentials!',
+          );
+        },
+      );
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
   }
 }
