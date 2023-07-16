3cfb2ebcad5102211f88e13e93369875fbbb808d
banku27 <pankajparihar123321@gmail.com>
2023-07-16 17:31:57 +0530

    signin method

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,6 +2,7 @@ const express=require("express");
 const User = require("../models/user");
 const bcryptjs=require("bcryptjs");
 const  authRouter=express.Router();
+const jwt=require('jsonwebtoken');
 
 authRouter.post("/api/signup",async(req,res)=>{
     try {
@@ -21,6 +22,29 @@ authRouter.post("/api/signup",async(req,res)=>{
         user=await user.save();
         res.status(200).json(user);
     } catch (e) {
+        res.status(500).json({error:e.message});
+    }
+}
+);
+
+authRouter.post('/api/signin',async(req,res)=>{
+
+    try {
+        const {email,password}=req.body;
+
+        const user= await User.findOne({email});
+        if (!user) {
+            return res.status(400).json({msg:"User with this email does not exist!"});
+        }
+
+      const isMatch= await bcryptjs.compare(password,user.password);
+      if (!isMatch) {
+        return res.status(400).json({msg:"Incorrect password!"});
+      }
+
+     const token= jwt.sign({id:user._id},"passwordKey");
+     res.json({   token, ...user._doc});
+    } catch (error) {
         res.status(500).json({error:e.message});
     }
 });
