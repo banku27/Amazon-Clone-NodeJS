09c089b125b8ad15e0d02a010ffb801ff9076962
banku27 <pankajparihar123321@gmail.com>
2023-07-18 17:15:57 +0530

    product schema

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,2 +1,14 @@
 const express=require("express");
 const adminRouter=express.Router();
+const admin=require('../middlewares/admin');
+
+//Add product
+
+adminRouter.post('/admin/add-product',admin, async (req,res)=>{
+
+    try {
+        const {name,description,images,quantity,price,category}=req.body
+    } catch (error) {
+        res.status(500).json({error:error.message});
+    }
+}); 
