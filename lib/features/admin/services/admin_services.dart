f675470323c8b59c0a78d6cd8bb8db6096620675
banku27 <pankajparihar123321@gmail.com>
2023-07-18 18:01:36 +0530

    product to MongoDB

--- lib/features/admin/services/admin_services.dart
+++ lib/features/admin/services/admin_services.dart
@@ -1,8 +1,14 @@
 import 'dart:io';
-
+import 'package:amazon_clone_nodejs/constants/error_handling.dart';
+import 'package:amazon_clone_nodejs/constants/utils.dart';
+import 'package:amazon_clone_nodejs/provider/user_provider.dart';
+import 'package:http/http.dart' as http;
 import 'package:amazon_clone_nodejs/models/product.dart';
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../../../constants/global_variables.dart';
 
 class AdminServices {
   void sellProduct({
@@ -14,6 +20,7 @@ class AdminServices {
     required String category,
     required List<File> images,
   }) async {
+    final user = Provider.of<UserProvider>(context, listen: false).user;
     try {
       final cloudinary = CloudinaryPublic('djxmjewgw', 'ikzuprj1');
       List<String> imageUrls = [];
@@ -31,6 +38,25 @@ class AdminServices {
         category: category,
         price: price,
       );
-    } catch (e) {}
+      http.Response res = await http.post(
+        Uri.parse('$uri/admin/add-product'),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+          'x-auth-token': user.token
+        },
+        body: product.toJson(),
+      );
+
+      // ignore: use_build_context_synchronously
+      httpErrorHandle(
+          response: res,
+          context: context,
+          onSuccess: () {
+            showSnackBar(context, 'Product Added Successfully');
+            Navigator.pop(context);
+          });
+    } catch (e) {
+      showSnackBar(context, e.toString());
+    }
   }
 }
