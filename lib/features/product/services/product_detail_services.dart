174e15bfa0a8c7fb5751f4052da581424e534e49
banku27 <pankajparihar123321@gmail.com>
2023-07-20 10:33:05 +0530

    rating model

--- lib/features/product/services/product_detail_services.dart
+++ lib/features/product/services/product_detail_services.dart
@@ -34,7 +34,9 @@ class ProductDetailsServices {
       httpErrorHandle(
         response: res,
         context: context,
-        onSuccess: () {},
+        onSuccess: () {
+          showSnackBar(context, 'Ratings Updated');
+        },
       );
     } catch (e) {
       showSnackBar(context, e.toString());
