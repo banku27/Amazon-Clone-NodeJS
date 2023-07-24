fde92e8ad11cfead16f53a3cb3deef8821531ece
banku27 <pankajparihar123321@gmail.com>
2023-07-24 18:56:25 +0530

    change order status from admin side

--- lib/features/orders/screens/order_details_screen.dart
+++ lib/features/orders/screens/order_details_screen.dart
@@ -35,6 +35,20 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
     currentStep = widget.order.status;
   }
 
+  // !!! ONLY FOR ADMIN!!!
+  void changeOrderStatus(int status) {
+    adminServices.changeOrderStatus(
+      context: context,
+      status: status + 1,
+      order: widget.order,
+      onSuccess: () {
+        setState(() {
+          currentStep += 1;
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = Provider.of<UserProvider>(context).user;
@@ -215,7 +229,8 @@ class _OrderDetailScreenState extends State<OrderDetailScreen> {
                     if (user.type == 'admin') {
                       return CustomButton(
                         text: 'Done',
-                        onTap: () {},
+                        color: Colors.amber,
+                        onTap: () => changeOrderStatus(details.currentStep),
                       );
                     }
                     return const SizedBox();
